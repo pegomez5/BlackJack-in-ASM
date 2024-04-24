c09bdb38e76ae6c3c67ad3650f0b5103f6609f8e
Timothy Jeon <159089769+noobytim@users.noreply.github.com>
2024-04-24 19:11:55 -0400

    Update TimothyJeonIPA3.s

--- TimothyJeonIPA3.s
+++ TimothyJeonIPA3.s
@@ -14,9 +14,12 @@ X_k: dw 1 ;accumulated value from prior iteration
 a: dw 11 ;a value that is odd co-prime to m
 m: dw 4133 ;a large prime
 
-
-start:
-    ;Random number generator
+def betInput {
+    mov a
+}
+;3.1. Representing cards, bets and wins on screen
+;Gets the random index
+def randomIndex {
     mov si, offset X_k ;moves offset of X_k to si
     mov ax, word [si] ;moves value of X_k to ax
     mov bp, offset m ;moves offset of m to bp
@@ -28,14 +31,34 @@ start:
     mov ax, dx ;moves remainder to ax
     mov cx, 52 ;moves 52 to cx where cx = 52
     div cx ;divides X_k by cx(52) where it'll be stored in ax
-    
-    ;Card value
+    ret
+}
+
+;3.1. Representing cards, bets and wins on screen
+;Gives card value from the random index
+def getCardValue {
     mov ax, dx ;moves remainder of dx to ax
     mov dx, 0 ;moves 0 to dx
     mov bx, 13 ;moves 13 to bx
     sub ax, 1 ;subtract 1 from ax
     div bx ;divides ax by bx
     add dx, 1 ;adds 1 to dx
+    ret
+}
+
+
+
+
+start:
+    call betInput
+    call randomIndex
+    call getCardValue
+    
+loop:
+    
+    call playerTurn
+    
+    call computerTurn
     
 
 
