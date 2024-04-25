82bed2282ee8713217ed05348b5c999e84d0b4dc
Timothy Jeon <159089769+noobytim@users.noreply.github.com>
2024-04-25 17:04:13 -0400

    Update TimothyJeonIPA3.s

--- TimothyJeonIPA3.s
+++ TimothyJeonIPA3.s
@@ -1,27 +1,25 @@
 ;CS274
 ;Timothy Jeon
-;For this program, we implemented code where the random input will give
-;a card from the standard deck of 52 cards. The 4 suits of clubs,
-;hearts, spades, and diamonds are represented by 0, 1, 2, and 3 which
-;will be shown by the A register while the card value will be shown by 
-;the D register which ranges from 1-13. 1 would be Ace while 11, 12, and 13
-;would be Jack, Queen, and King. Some examples would be the random input 
-;of 31 giving the 5 of spades and the random input of 15 giving the 2 of hearts.
 
 ;data
 X_0: dw 7 ;initial value
 X_k: dw 1 ;accumulated value from prior iteration
 a: dw 11 ;a value that is odd co-prime to m
 m: dw 4133 ;a large prime
+playerBet: dw 55 ;hexadecimal value of 55 is 37 shown in memory
+playerWins: dw 0
+money: dw 1000
 
 def betInput {
-    mov a
+    mov ah, 0x01
+    int 0x21
+    mov word [offset playerBet], ax
+    ret
 }
+
 ;3.1. Representing cards, bets and wins on screen
 ;Gets the random index
 def randomIndex {
-    mov dx, 0
-    
     mov si, offset X_k ;moves offset of X_k to si
     mov ax, word [si] ;moves value of X_k to ax
     mov bp, offset m ;moves offset of m to bp
@@ -48,6 +46,21 @@ def getCardValue {
     ret
 }
 
+def playerTurn {
+
+    ret
+}
+
+def computerTurn {
+
+    ret
+}
+
+def determineWinner {
+
+    ret
+}
+
 
 
 
@@ -56,12 +69,11 @@ start:
     call randomIndex
     call getCardValue
     
+    
 loop:
-    
     call playerTurn
-    
     call computerTurn
-    
+    call determineWinner
 
 
 
